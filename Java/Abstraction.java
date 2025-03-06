8cb120967ff644fe8941120156e8bda558343144
gauravKothari <kotharigaurav6@gmail.com>
2025-03-06 17:42:25 +0530

    js

---
+++ Java/Abstraction.java
@@ -0,0 +1,21 @@
+Abstraction:
+------------
+1.Abstraction is the process of hiding implementation details and showing only functionalities to the user.
+
+2.Abstraction can be achieved with the help of abstract class.
+
+3.With the help of abstract class, we can achieve 0 to 100% abstraction.
+
+4.Abstract class can contains abstract method(method with no definition part), concrete method(method having definition part) and constructor.
+
+5.For making any class as an abstract class, we need to put abstract keyword followed by class name along with this for making any method as an abstract method , we needs to put abstract keyword followed by method name.
+
+6.For making any class as an abstract class, its not mandatory to have abstract method inside it, abstract class can be created by simply putting abstract keyword in front of class name.
+
+7.Abstract class cannot be instantiated, which means we cannot create object of abstract class.
+
+8.Abstract class only provides reference to there child classes, where child classes may create there own object in reference with that abstract class.
+
+9.It mandatory for the child class which extends abstract parent class(which contains abstract method), to give the implementation of abstract method otherwise it will give you compilation error.
+
+10.If we want to achieve some of the functionalities forcefully from the child class, then in that case we use abstract class.
