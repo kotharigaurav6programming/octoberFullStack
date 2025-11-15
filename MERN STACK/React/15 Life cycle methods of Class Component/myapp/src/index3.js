79b35bffa799a1f7e41463c3ce358f2c9c598c23
gauravKothari <kotharigaurav6@gmail.com>
2025-11-15 19:56:22 +0530

    react

---
+++ MERN STACK/React/15 Life cycle methods of Class Component/myapp/src/index3.js
@@ -0,0 +1,25 @@
+import React from 'react';
+import {createRoot} from 'react-dom/client';
+
+class Header extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {favoritecolor: "red"};
+  }
+  static getDerivedStateFromProps(props, state) {
+    console.log("executes : ",state.favoritecolor);
+    return {favoritecolor: props.favcol };
+  }
+  changeColor = () => {
+    this.setState({favoritecolor: "blue"});
+  }
+  render() {
+    return (
+      <div>
+      <h1>My Favorite Color is {this.state.favoritecolor}</h1>
+      <button type="button" onClick={this.changeColor}>Change color</button>
+      </div>
+    );
+  }}
+
+createRoot(document.getElementById("root")).render(<Header favcol="yellow"/>); 
